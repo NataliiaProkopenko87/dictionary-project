f215024cfc0c7ca9f497b3662021ca0a6cc54dd8
NataliiaProkopenko87 <nataliiaprokopenko@yahoo.com>
2022-09-17 10:49:39 +0200

    Added search form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
-        Hello
+       <main>
+        <Dictionary />
+       </main>
+       <footer className="App-footer"><small>Coded by Nataliia Prokopenko, open-soursed on GitHub</small></footer>
+       </div>
     </div>
   );
 }
